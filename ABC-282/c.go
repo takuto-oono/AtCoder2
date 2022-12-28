8e80bba7fe03d9028b17866cca789f15c2d8c67b
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-12-28 10:46:32 +0900

    add: golang string debug

--- ABC-282/c.go
+++ ABC-282/c.go
@@ -1,89 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
-var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
-
-func stringInput() string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func intInput() int {
-	sc.Scan()
-	x, err := strconv.Atoi(sc.Text())
-	if err != nil {
-		panic(err)
-	}
-	return x
-}
-
-func intSliceInput(n int) []int {
-	sl := []int{}
-	for i := 0; i < n; i++ {
-		x := intInput()
-		sl = append(sl, x)
-	}
-	return sl
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func initSlSl(y, x int) [][]int {
-	sl := make([][]int, y)
-	if x == -1 {
-		for i := 0; i < y; i++ {
-			sl[i] = []int{}
-		}
-		return sl
-	}
-	for i := 0; i < y; i++ {
-		sl[i] = make([]int, x)
-	}
-	return sl
-}
-
-func init() {
-	sc.Split(bufio.ScanWords)
-}
-
 func main() {
-	n := intInput()
-	var s string
+	var n int
+	var s []byte
+	fmt.Scan(&n)
 	fmt.Scan(&s)
-	cntDoubleQuotation := 0
-	var ansSl []byte
-	for i := 0; i < n; i++ {
-		if s[i:i+1] == "," {
-			if cntDoubleQuotation%2 == 0 {
-				ansSl = append(ansSl, '.')
-			} else {
-				ansSl = append(ansSl, ',')
-			}
-		} else if s[i:i+1] == "\"" {
-			cntDoubleQuotation++
-			ansSl = append(ansSl, '"')
-		} else {
-			ansSl = append(ansSl, s[i])
+	flag := true
+	for i := 0; i < n; i ++ {
+		if s[i] == '"' {
+			flag = !flag
+		}
+		if s[i] == ',' && flag {
+			s[i] = '.'
 		}
 	}
-	fmt.Println(string(ansSl))
+	fmt.Println(string(s))
 }
--- abc-283/c.go
+++ abc-283/c.go
@@ -1,83 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
-var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
-
-func stringInput() string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func intInput() int {
-	sc.Scan()
-	x, err := strconv.Atoi(sc.Text())
-	if err != nil {
-		panic(err)
-	}
-	return x
-}
-
-func intSliceInput(n int) []int {
-	sl := []int{}
-	for i := 0; i < n; i++ {
-		x := intInput()
-		sl = append(sl, x)
-	}
-	return sl
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func initSlSl(y, x int) [][]int {
-	sl := make([][]int, y)
-	if x == -1 {
-		for i := 0; i < y; i++ {
-			sl[i] = []int{}
-		}
-		return sl
-	}
-	for i := 0; i < y; i++ {
-		sl[i] = make([]int, x)
-	}
-	return sl
-}
-
-func init() {
-	sc.Split(bufio.ScanWords)
-}
 
 func main() {
-	s := stringInput()
+	var s []byte
+	fmt.Scan(&s)
 	ans := len(s)
 	isBeforeZero := false
-
 	for _, c := range s {
-		if c == 48 {
+		if c == '0' {
 			if isBeforeZero {
-				isBeforeZero = false
 				ans -= 1
-			} else {
-				isBeforeZero = true
 			}
+			isBeforeZero = !isBeforeZero
 		} else {
 			isBeforeZero = false
 		}
