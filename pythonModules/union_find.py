e5e6f7d5dccc228b4f433cc1245d450d21816c29
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-02 17:05:57 +0900

    add: Union_Find class by Python

---
+++ pythonModules/union_find.py
@@ -0,0 +1,34 @@
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.parents = [-1 for _ in range(n)]
+        self.sizes = [1 for _ in range(n)]
+
+    def get_parent(self, x: int) -> int:
+        if self.parents[x] == -1:
+            return x
+        self.parents[x] = self.get_parent(self.parents[x])
+        return self.parents[x]
+
+    def is_same(self, x: int, y: int) -> bool:
+        return self.get_parent(x) == self.get_parent(y)
+
+    def unite(self, x: int, y: int) -> bool:
+        x, y = self.get_parent(x), self.get_parent(y)
+        if x == y:
+            return False
+        if self.sizes[x] < self.sizes[y]:
+            x, y = y, x
+        self.parents[y] = x
+        self.sizes[x] += self.sizes[y]
+        return True
+
+    def get_size(self, x: int) -> int:
+        return self.sizes[self.get_parent(x)]
+
+    def get_parents_count(self) -> int:
+        cnt = 0
+        for parent in self.parents:
+            if parent == -1:
+                cnt += 1
+        return cnt
+
