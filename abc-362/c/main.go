c64eecc2cf7df80d78fbfdd7587f0ff9e0c057d7
takuto-oono <takutoono053037@gmail.com>
2024-08-12 11:47:59 +0900

    add c.go

---
+++ abc-362/c/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	n := inputIntSl()[0]
+	lrSl := inputIntSlSl(n)
+
+	lSum, rSum := 0, 0
+	for _, lr := range lrSl {
+		lSum += lr[0]
+		rSum += lr[1]
+	}
+	if lSum > 0 || rSum < 0 {
+		fmt.Println("No")
+		os.Exit(0)
+	}
+
+	fmt.Println("Yes")
+
+	ans := make([]int, n)
+	sum := lSum
+	for i, lr := range lrSl {
+		ans[i] = lr[0]
+	}
+
+	for i, lr := range lrSl {
+		if sum == 0 {
+			break
+		}
+
+		if sum > 0 {
+			panic("sum > 0")
+		}
+
+		if 0-sum >= lr[1]-lr[0] {
+			ans[i] = lr[1]
+			sum += lr[1] - lr[0]
+		} else {
+			ans[i] = lr[0] - sum
+			sum = 0
+		}
+	}
+
+	printIntSlice(ans)
+}
+
+func inputIntSl() []int {
+	sc.Scan()
+	inputs := strings.Split(sc.Text(), " ")
+	result := make([]int, len(inputs))
+	for i, input := range inputs {
+		result[i] = stringToInt(input)
+	}
+	return result
+}
+
+func inputStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func inputIntSlSl(n int) [][]int {
+	results := make([][]int, 0)
+
+	for i := 0; i < n; i++ {
+		if !sc.Scan() {
+			break
+		}
+		result := make([]int, 0)
+		for _, input := range strings.Split(sc.Text(), " ") {
+			result = append(result, stringToInt(input))
+		}
+		results = append(results, result)
+	}
+
+	return results
+}
+
+func inputStrSlSl(n int) [][]string {
+	results := make([][]string, 0)
+
+	for i := 0; i < n; i++ {
+		if !sc.Scan() {
+			break
+		}
+		results = append(results, strings.Split(sc.Text(), " "))
+	}
+
+	return results
+}
+
+func inputCharSlSl(n int) [][]string {
+	results := make([][]string, 0)
+
+	for i := 0; i < n; i++ {
+		if !sc.Scan() {
+			break
+		}
+		results = append(results, strings.Split(sc.Text(), ""))
+	}
+
+	return results
+}
+
+func printIntSlice(sl []int) {
+	strSl := make([]string, len(sl))
+	for i, num := range sl {
+		strSl[i] = strconv.Itoa(num)
+	}
+	fmt.Println(strings.Join(strSl, " "))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxSl(sl []int) int {
+	result := math.MinInt
+	for _, x := range sl {
+		result = max(result, x)
+	}
+	return result
+}
+
+func minSl(sl []int) int {
+	result := math.MaxInt
+	for _, x := range sl {
+		result = min(result, x)
+	}
+	return result
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
+
+func stringToInt(s string) int {
+	x, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func intToString(x int) string {
+	return strconv.Itoa(x)
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func binarySearch(sl []int, v int) int {
+	l, r := 0, len(sl)-1
+
+	for l < r {
+		m := (l + r) / 2
+		if sl[m] == v {
+			return m
+		} else if sl[m] < v {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return -1
+}
