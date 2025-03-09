77d426a73e30da1ffc0ea7aa397c353765baa675
takuto-oono <takutoono053037@gmail.com>
2025-03-09 19:01:54 +0900

    feat: abc393のaとbを回答

---
+++ abc-393/a/a.py
@@ -0,0 +1,15 @@
+def main():
+    s1, s2 = input().split()
+
+    if s1 == "sick" and s2 == "sick":
+        print(1)
+    elif s1 == "sick" and s2 == "fine":
+        print(2)
+    elif s1 == "fine" and s2 == "sick":
+        print(3)
+    else:
+        print(4)
+
+
+if __name__ == "__main__":
+    main()
